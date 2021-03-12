522a137872aad99fbe9ed40a9882782ad7de9c85
AdrianValenzuela <avgvalenzuela@gmail.com>
2021-03-12 18:38:08 +0100

    fix import user service in NewAdOrLoginController

--- js/controllers/NewAdOrLoginController.js
+++ js/controllers/NewAdOrLoginController.js
@@ -1,4 +1,4 @@
-import userService from "../services/userService.js";
+import userService from "../services/UserService.js";
 import BaseController from "./BaseController.js";
 
 export default class NewAdOrLoginController extends BaseController {
